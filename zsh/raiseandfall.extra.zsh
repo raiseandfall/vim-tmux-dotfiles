16cb019b78cc4b4939410d85acdf30a8b640851a
raiseandfall <druidev+github@gmail.com>
2015-06-22 11:02:13 -0700

    Updated git email address

--- zsh/raiseandfall.extra.zsh
+++ zsh/raiseandfall.extra.zsh
@@ -5,7 +5,7 @@ source ~/.nvm/nvm.sh
 GIT_AUTHOR_NAME="raiseandfall"
 GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 git config --global user.name "$GIT_AUTHOR_NAME"
-GIT_AUTHOR_EMAIL="druidev+github@gmail.com"
+GIT_AUTHOR_EMAIL="matthieu@johnmcneilstudio.com"
 GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
 
