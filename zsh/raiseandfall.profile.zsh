50fb4af074c547a234b7219916794189cccad96f
raiseandfall <druidev+github@gmail.com>
2015-06-22 12:04:03 -0700

    Added karn

--- zsh/raiseandfall.profile.zsh
+++ zsh/raiseandfall.profile.zsh
@@ -1,3 +1,7 @@
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 
 export PYENV_ROOT=/usr/local/var/pyenv
+export GOPATH=$HOME/go
+
+# Karn overrides git ( for multiple identities )
+if which karn > /dev/null; then eval "$(karn init)"; fi
